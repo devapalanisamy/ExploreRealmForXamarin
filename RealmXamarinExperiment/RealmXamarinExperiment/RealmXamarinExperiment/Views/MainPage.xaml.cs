c9825cda5f6167cbc9fa90b1b2751d1345f6663d
SOFTWARE\deva.palanisamy <deva.palanisamy@innovise.com>
2017-09-07 14:17:05 +0100

    created initial template

---
+++ RealmXamarinExperiment/RealmXamarinExperiment/RealmXamarinExperiment/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RealmXamarinExperiment.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
